65709ddfd7aa83e6d1682ec1f6b8b42e7a02eeb3
ratanon-144 <63015144@kmitl.ac.th>
2022-05-04 21:43:46 +0700

    aa

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const port ='8080';
+const port = '8080';
 const express = require('express');
 const path = require('path');
 const cookieSession = require('cookie-session');
@@ -8,75 +8,82 @@ const { body, validationResult } = require('express-validator');
 
 const app = express();
 app.use(express.static(__dirname + '/scr'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 // SET OUR VIEWS AND VIEW ENGINE
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // APPLY COOKIE SESSION MIDDLEWARE
 app.use(cookieSession({
     name: 'session',
     keys: ['key1', 'key2'],
-    maxAge:  3600 * 1000 // 1hr
+    maxAge: 3600 * 1000 // 1hr
 }));
-var session_mode = 0 ;
-var name_is = '' ;
+var session_mode = 0;
+var name_is = '';
 
 ////////////////////////////////////////////////////////////////////////////////////////
 app.get('/', function (req, res) {
-  //  console.log('index');
-   // console.log(session_mode);
-  //  console.log(req.session.isLoggedIn);
+    //  console.log('index');
+    // console.log(session_mode);
+    //  console.log(req.session.isLoggedIn);
     if (!req.session.isLoggedIn) {
         console.log("NO");
-        session_mode =0;
+        session_mode = 0;
         name_is = '';
         res.render("index", { age: session_mode });
-    }else if (req.session.isLoggedIn) {
-    //    console.log("LOG");
-        session_mode =10;
-        dbConnection.execute("SELECT * FROM `userDetail` WHERE `id`=?", [req.session.userID])
-        .then(([rows]) => {
-            // console.log([rows]);
-            name_is =rows[0].name;
-            res.render('index', {
-                age:session_mode ,
-                name: rows[0].name,
-                lastname: rows[0].lastname,
-                email: rows[0].email
+    } else if (req.session.isLoggedIn) {
+        //    console.log("LOG");
+        session_mode = 10;
+       // dbConnection.execute("SELECT * FROM `userDetail` WHERE `id`=?", [req.session.userID])
+        dbConnection.execute("SELECT * FROM `touristAttractionDetail` WHERE `TID`= ?",[1])
+            .then(([rows]) => {
+                // console.log([rows]);
+                name_is = rows[0].name;
+                res.render('index', {
+                    age: session_mode,
+                    name: rows[0].lastname,
+                    lastname: rows[0].lastname,
+                    email: rows[0].email
+
+                    
+                });
             });
-        });
     }
 });
 
 app.get('/login', function (req, res) {
     // console.log('login');
     // console.log(session_mode);
-    res.render("login",{ age: 0 });
-   
+    res.render("login", { age: 0 });
+
 });
- 
+
 app.get('/register', function (req, res) {
     // console.log('USER API register');
     // console.log(session_mode);
     res.render("register", { age: 0 });
-   
+
 });
 
-app.get('/map', function (req, res) {
-    // console.log('USER API map');
-    res.render("map",{ 
-        age:session_mode ,
-        name:name_is
-    });
-    
-});
 
 app.get('/logout', (req, res) => {
     //session destroy
     req.session = null;
     res.redirect('/');
+});
+
+app.get('/map', (req, res) => {
+
+    res.render('map',{age: 0, 
+        region: '' ,
+        province: '' ,
+        contactNumber: ''
+
+    
+    });
+
 });
 
 ///////////////////////////POST ///////////////////////////////////////////////
@@ -86,7 +93,8 @@ app.post('/login', [
         // console.log(value);
         return dbConnection.execute('SELECT email FROM userDetail WHERE email=?', [value])
             .then(([rows]) => {
-                // console.log(rows.length);
+                console.log(rows);
+                console.log(rows.length);
                 if (rows.length == 1) {
                     return true;
 
@@ -115,7 +123,7 @@ app.post('/login', [
                     }
                     else {
                         res.render('login', {
-                            age:0,
+                            age: 0,
                             login_errors: ['Invalid Password!']
                         });
                     }
@@ -136,7 +144,7 @@ app.post('/login', [
         // REDERING login-register PAGE WITH LOGIN VALIDATION ERRORS
         // console.log(allErrors);
         res.render('login', {
-            age:0,
+            age: 0,
             login_errors: allErrors
         });
     }
@@ -162,17 +170,17 @@ app.post('/register',
     (req, res, next) => {
 
         const validation_result = validationResult(req);
-        const { user_name,user_lastname, user_pass, user_email, user_tell } = req.body;
+        const { user_name, user_lastname, user_pass, user_email, user_tell } = req.body;
         // console.log(req.body);
         // IF validation_result HAS NO ERROR
         if (validation_result.isEmpty()) {
             // password encryption (using bcryptjs)
             bcrypt.hash(user_pass, 12).then((hash_pass) => {
                 // INSERTING USER INTO DATABASE
-                dbConnection.execute("INSERT INTO `userDetail`(`name`,`lastname`,`email`,`password`,`tell`) VALUES(?,?,?,?,?)", [user_name,user_lastname,user_email, hash_pass, user_tell])
+                dbConnection.execute("INSERT INTO `userDetail`(`name`,`lastname`,`email`,`password`,`tell`) VALUES(?,?,?,?,?)", [user_name, user_lastname, user_email, hash_pass, user_tell])
                     .then(result => {
-                        res.render('login',{
-                            age:0
+                        res.render('login', {
+                            age: 0
                         });
                     }).catch(err => {
                         // THROW INSERTING USER ERROR'S
@@ -191,18 +199,42 @@ app.post('/register',
             });
             // REDERING login-register PAGE WITH VALIDATION ERRORS
             res.render('register', {
-                age:0,
+                age: 0,
                 register_error: allErrors,
                 old_data: req.body
             });
         }
     });// END OF REGISTER PAGE
 
+
+    app.post('/map', (req, res,next) => {
+
+        console.log("map");
+        session_mode = 1;
+        const validation_result = validationResult(req);
+        const { check=0 } = req.body;
+        console.log("CHECK : %s",check);
+        if (validation_result.isEmpty()) {
+        dbConnection.execute("SELECT * FROM `touristAttractionDetail` WHERE `TID`= ?",[check]).then(([rows]) => {
+           // console.log([rows]);
+            console.log(rows[0].region);
+            res.render('map', {
+                age: session_mode,
+                region: rows[0].region,
+                province: rows[0].province,
+                contactNumber: rows[0].contactNumber
+
+                
+            });
+        });
+        }
+    });
+    
 ///////////////////////////END POST ///////////////////////////////////////////////
 app.use('/', (req, res) => {
     res.status(404).send('<h1>404 Page Not Found!</h1>');
 });
 app.listen(port, function (err) {
     if (err) console.log(err);
-    console.log("Server listening on %s",port);
+    console.log("Server listening on %s", port);
 });
